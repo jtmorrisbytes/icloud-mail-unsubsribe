ba18a6b209c06488369fc12a5c9d46ef1831bfd8
Jordan Morris <jhecybertinkerer@gmail.com>
2020-03-10 12:34:24 -0500

    making progress... found the unsubscribe link

--- index.js
+++ index.js
@@ -11,22 +11,42 @@ const config = {
     tls: true
   }
 };
+
+let approvedSenders = [
+  "no-reply@accounts.google.com",
+  "bestbuycard@info6.accountonline.com"
+];
+
 imaps.connect(config).then(function handleConnectionSuccess(connection) {
   return connection.openBox("INBOX").then(() => {
-    var delay = 24 * 3600 * 1000;
-    var yesterday = new Date();
-    yesterday.setTime(Date.now() - delay);
-    yesterday = yesterday.toISOString();
-    const searchCriteria = ["SEEN", ["SINCE", yesterday]];
+    var delay = 24 * 3600 * 1000 * 4;
+    var lastYear = new Date();
+    lastYear.setTime(Date.now() - delay);
+    lastYear = lastYear.toISOString();
+    const searchCriteria = ["UNSEEN", ["SINCE", lastYear]];
     const fetchOptions = {
-      bodies: ["HEADER"],
+      bodies: ["HEADER", "TEXT"],
       markSeen: false
     };
     connection
       .search(searchCriteria, fetchOptions)
       .then(function parseResult(results) {
         results.forEach(result => {
-          console.dir(result);
+          let header = result.parts[0];
+          console.log("Sender", header.body.sender);
+          console.log("from", header.body.from);
+          console.log("Subject", header.body.subject);
+          //   console.log("Unsubscribe email", header["list-unsubscribe"]);
+          let text = result.parts[1];
+          //   console.log(result.parts[0].body);
+          try {
+            if (result.parts[0].body["list-unsubscribe"]) {
+              console.log(
+                "list-unsubscribe",
+                result.parts[0].body["list-unsubscribe"]
+              );
+            }
+          } catch (e) {}
         });
       });
   });
